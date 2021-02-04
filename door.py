156616c2604cb82ab0d0f91cf685695823ee888c
Starcly56 <humagainuzjwol@gmail.com>
2021-02-04 17:43:25 +0545

    algorithm to close and open the door based on the visual captured

---
+++ door.py
@@ -0,0 +1,25 @@
+#Importing file for servocontrolling function
+import servocontrol
+
+
+#Function for unlocking the door
+def unlock():
+    try: 
+        print("Unlocking door...")
+        #setting angle at 180
+        servocontrol.SetAngle(180)
+        print("Door is unlocked")
+        return "Door is unlocked"
+    except:
+        print("The door was not correctly unlocked")
+    
+#Function for locking the door    
+def lock():
+    try:
+        print("Locking door...")
+        #setting angle at 0
+        servocontrol.SetAngle(0)
+        print("Door is locked")
+        return "Door is locked"
+    except:
+        print("The door was not correcly locked")
